a8786376f4f3f5d1a8b5b2bc3482dc0ec884da9f
José María Santos Fernández <jm.santos@innovation.es>
2021-08-08 18:41:21 +0200

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 80,
+  useTabs: false,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'strict',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  proseWrap: 'never',
+  quoteProps: 'as-needed',
+  trailingComma: 'none',
+  vueIndentScriptAndStyle: false
+};
