e10288574ac55c0ea0293401ded78eefbb15329d
José María Santos Fernández <jm.santos@innovation.es>
2021-08-09 15:10:15 +0200

    añado lógica de nuevos jugadores

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          :src="require('./assets/girl.svg')"          
-          contain
-          height="50"
-        />
-        Julititi´s scores       
-          
-      </div>
-
-      <v-spacer></v-spacer>
-
-      
+    <v-app-bar app color="primary" dark>
+      <v-btn to="/" text>
+        <v-toolbar-title>Julititi´s scores</v-toolbar-title>        
+      </v-btn>
+      <v-spacer />
+      <div>
+          <v-img :src="require('./assets/girl.svg')" contain height="50" width="50" />
+        </div>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
-    <v-bottom-navigation
-    v-model="value"
-    :background-color="color"
-    dark
-    shift
-    
-  >
-  
-    <v-btn to="/" >
-      <span>Partidas</span>
+    <v-bottom-navigation v-model="value" :background-color="color" dark shift>
+      <v-btn to="/">
+        <span>Partidas</span>
 
-      <v-icon>mdi-gamepad-square</v-icon>
-    </v-btn>
-  
-  
-    <v-btn to="/adversaries">
-      <span>Adversarios</span>
+        <v-icon>mdi-gamepad-square</v-icon>
+      </v-btn>
 
-      <v-icon>mdi-account-supervisor-circle</v-icon>
-    </v-btn> 
-  
-  </v-bottom-navigation>
+
+      <v-btn to="/adversaries">
+        <span>Adversarios</span>
+
+        <v-icon>mdi-account-supervisor-circle</v-icon>
+      </v-btn>
+
+    </v-bottom-navigation>
   </v-app>
 </template>
 
@@ -54,17 +37,17 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'App',
 
-  data: () => ({ value: 1 }),
+  data: () => ({ value: 0 }),
 
     computed: {
       color () {
-        switch (this.value) {
-          case 0: return 'blue-grey'
-          case 1: return 'teal'
-          case 2: return 'brown'
-          case 3: return 'indigo'
-          default: return 'blue-grey'
+        const color: {[key: string]: string} = {
+          0: 'blue-grey',
+          1: 'teal',
+          2: 'brown',
+          3: 'indigo',
         }
+        return color[this.value] || 'blue-grey'
       },
     },
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <section>
     <v-container>
       <v-row dense>
-        <v-col cols="6" md="3">
+        <v-col cols="12" md="3">
           <v-card color="#385F73" dark>
             <v-card-title class="text-h5">
               Añadir nuevo juego
@@ -11,14 +11,14 @@
             <v-card-subtitle>Hacer click aquí para empezar el registro de un nuevo juego.</v-card-subtitle>
 
             <v-card-actions>
-              <v-btn block x-large>
+              <v-btn block x-large @click="dialog = !dialog">
                 Nueva partida
               </v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
 
-        <v-col v-for="(item, i) in items" :key="i" cols="6" md="3">
+        <v-col v-for="(item, i) in items" :key="i" cols="12" md="3">
           <v-card :color="item.color" dark>
             <div class="d-flex flex-no-wrap justify-space-between">
               <div>
@@ -39,21 +39,60 @@
         </v-col>
       </v-row>
     </v-container>
+    <v-dialog
+        v-model="dialog"
+        max-width="500px"
+      >
+        <v-card>
+          <v-card-title>
+            Dialog 2
+          </v-card-title>
+          <v-card-text>
+            
+            <v-select
+              :items="select"
+              label="A Select List"
+              item-value="text"
+            ></v-select>            
+            <v-btn v-for="(player, index) in playerInGame" :key="player" @click="deletePlayer(index)">{{player}}</v-btn>
+          </v-card-text>
+          <v-divider />
+          <v-col
+            cols="12"            
+          >
+            <v-text-field
+              label="Solo"
+              placeholder="Placeholder"
+              solo
+              aria-autocomplete="false"
+              v-model="player"
+            ></v-text-field>
+            <v-btn text @click="addNewPlayer()">Añadir</v-btn>
+          </v-col>
+          <v-card-actions>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
   </section>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+  import { Component, Vue } from 'vue-property-decorator';
+
   //import HelloWorld from '../components/HelloWorld.vue'
 
-  export default Vue.extend({
-    name: 'Home',
-
-    components: {
-      //HelloWorld,
-    },
-    data: () => ({
-      items: [
+  @Component({
+    name: 'Home'
+  })
+  export default class Home extends Vue {
+    items= [
         {
           color: '#1F7087',
           src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
@@ -65,8 +104,26 @@
           src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
           title: 'Halcyon Days',
           artist: 'Ellie Goulding',
-        },
-      ],
-    }),
-  })
+        },        
+      ]
+      dialog= false
+      player= ''
+      select=[
+        { text: 'Baraja española' },
+        { text: 'Uno' },
+        { text: 'Dobble' },
+      ]
+
+      playerInGame: string[] = []
+
+      addNewPlayer(): void{        
+        this.playerInGame.push(this.player)
+        this.player = '';
+      }
+      deletePlayer(index: number):void{
+        this.playerInGame.splice(index, 1)
+      }
+  }
+
+  
 </script>
